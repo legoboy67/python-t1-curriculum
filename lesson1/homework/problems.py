ac8ca74cdf92341d6830818fb86fc6d127b1c2dc
legoboy67 <ishanjaved.s@gmail.com>
2025-10-30 01:10:15 +0000

    Lesson 1 Homework

--- lesson1/homework/problems.py
+++ lesson1/homework/problems.py
@@ -1,24 +1,35 @@
 # Problem 1
 # Create a variable for your lucky number and print it.
-
+luckyNumber = 7
+print(luckyNumber)
 
 
 # Problem 2
 # Create a variable for the number of hours you sleep and print it.
-
+sleepHours = 10
+print(sleepHours)
 
 
 # Problem 3
 # Create a variable for your favorite fruit and print it in a full sentence.
-
+favoriteFruit = "Watermelon"
+print(favoriteFruit)
 
 
 # Problem 4
 # Create two variables: one for your city and one for your country.
 # Print them on 2 separate lines.
-
+homeCity = "Redmond"
+homeCountry = "U.S.A."
+print(homeCity)
+print(homeCountry)
 
 
 # Problem 5
 # Create 3 variables: your pet's name, its age, and its type.
 # Print them on the same line in a full sentence.
+print("This pet is made up because I do not have one")
+petName = "Dakota"
+petAge = "2 dog years"
+petType = "Corgi"
+print("I would want a pet named ", petName, ". It would be ", petAge, " and it would be a ", petType, ".")
