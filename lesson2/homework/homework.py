9764e081d0f14e699ccd4fb9f2998a107078b29f
legoboy67 <ishanjaved.s@gmail.com>
2025-11-09 22:36:36 +0000

    Problems

--- lesson2/homework/homework.py
+++ lesson2/homework/homework.py
@@ -1,26 +1,67 @@
 # Homework Problem 1
 # Ask the user for two numbers.
 # Print their quotient and remainder on separate lines.
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+
+quotient = num1 // num2
+remainder = num1 % num2
+
+print("The quotient is:", quotient)
+print("The remainder is:", remainder)
+
+
 
 
 
 # Homework Problem 2
 # Ask the user for their favorite animal and favorite color.
 # Print a sentence combining them like: "A blue tiger would be awesome!"
+favouriteColor = input("What is your favourite color?")
+favouriteAnimal = input("What is your favourite animal?")
+print("A ",favouriteColor,favouriteAnimal," would super duper awesome, don't you think?!")
 
 
 
 # Homework Problem 3
 # Use a for loop to print all the even numbers from 0 to 10 (including 10).
+for i in range(2, 12, 2):
+    print(i)
 
 
 
 # Homework Problem 4
 # Ask the user how many push-ups they can do.
 # Multiply it by 7 and print how many they could do in a week.
-
+numeropushups = int(input("How many push-ups can you do in one day? "))
+pushupperweek = numeropushups * 7
+print("Did you know that you can do", pushupperweek, "push-ups in a week if you do", numeropushups, "per day?")
 
 
 # Homework Problem 5
 # Use a for loop to print the square of each number from 1 to 6.
 # (Example: 1*1=1, 2*2=4, etc.)
+for i in range(1, 7):
+    print(i, "x", i, "=", i * i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#My sister did the stuff on line 66-67.
+#Minecraft stack
+#LAVA CHICKEN 65!
+for i in range(1, 68):
+    print(i)
--- main.py
+++ main.py
@@ -1,1 +1,1 @@
-from lesson1.homework import problems
+from lesson2.homework import problems
